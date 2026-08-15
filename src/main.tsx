ce3edaf1355edaf14958d167dfb7ba95052d4fb8
sisaiyahardeep <sisaiyahardeep@gmail.com>
2026-08-15 20:18:53 +0530

    refactor: switch to HashRouter for static hosting
    Update the router to HashRouter and set the base path to relative to ensure the application works correctly when deployed to static hosting environments.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
@@ -8,9 +8,9 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </StrictMode>,
 );
